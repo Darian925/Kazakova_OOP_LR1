c399cd8df0edeb3bcb2c43d46ff499e6f23b0f68
Darian925 <kazakovadaria150@yandex.ru>
2025-11-11 14:02:22 +0300

    Добавьте файлы проекта.

---
+++ Kazakova_OOP_LR1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kazakova_OOP_LR1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Название и номер лабораторной работы: Лабораторная работа номер 1. Разработка консольного приложения ");
+            Console.WriteLine("ФИО студента: Казакова Дарья Владимировна");
+            Console.WriteLine("Группа студента и шифр специальности: ИДБ-23-01 , 09.03.01");
+            Console.WriteLine("Дата рождения студента: 31.03.2005");
+            Console.WriteLine("Населенный пункт постоянного места жительства студента: Москва");
+            Console.WriteLine("Любимый предмет в школе: Изо");
+            Console.WriteLine("Краткое описание хобби,увлечений,интересов: Манга и компьютерные игры ");
+            Console.ReadKey();
+        }
+    }
+}
